4158dca22abcfe01fb522af65e95330510f3be63
SumeyraAgambayev <sumeyra.agambayev@thebankoflondon.com>
2023-04-13 04:09:00 -0500

    if else to verify element presence

--- src/firstSteps/Basics1.java
+++ src/firstSteps/Basics1.java
@@ -1,7 +1,12 @@
 package firstSteps;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Basics1 {
 
@@ -15,18 +20,41 @@ public class Basics1 {
 
 				Thread.sleep(1000);
 
-				//command 
+				
+				 
 
 				driver.findElement(By.xpath("//input[@name='username']")).sendKeys("Admin");
 				driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin123");
 				driver.findElement(By.xpath("//button[@type='submit']")).click();
 
+				
+				Thread.sleep(1000);
 
-				//driver.get("https://news.google.com/home?hl=en-US&gl=US&ceid=US:en");
+				driver.findElement(By.xpath("//span[text()='Leave']")).click();
+				Thread.sleep(1000);
 
-				//List <WebElement> links = driver.findElements(By.tagName("a"));
+				driver.findElement( By.xpath("//div[text()='Select']")).click();
 
+			
 
+				List<WebElement> menuItems = driver.findElements(( By.xpath("//div[@role='listbox']")));	
+
+				//WebDriverWait wait = new WebDriverWait(driver, 20);
+				//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Rejected']")));
+
+				String element = "Rejected";
+				
+				for(WebElement item:menuItems) {
+
+					if(item.getText().contains(element)) {
+					
+						System.out.println(element + " is present");
+					
+					}else {
+						System.out.println(element + " is not present");
+					}
+					
+		}
 
 	}
 
